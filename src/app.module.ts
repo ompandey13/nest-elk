4b1cbe02d8f817c76de8f04fa5f2e21360b053c2
ompandey13 <om.pandey@crownstack.com>
2021-06-23 14:29:01 +0530

    elastic search initial setup

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import Joi from 'joi';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string().required(),
+        NODE_ENV: Joi.string().required(),
+        PORT: Joi.number(),
+        ELASTICSEARCH_NODE: Joi.string().required(),
+      }),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
